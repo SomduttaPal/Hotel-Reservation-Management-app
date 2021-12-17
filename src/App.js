3f408ca19ee94df688059cf68052b1c9ace7e547
Somdutta Pal <somdutta_pal@persistent.com>
2021-12-17 13:44:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+import { Application } from './Application.jsx';
+function App(){
+  return <Application/>
+};
+
+export default App;
---
+++ src/Application.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from "./Home";
+import About from "./About";
+import Service from "./Service";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+import { Route, Routes } from 'react-router-dom';
+  
+const Application = () => {
+    return (
+        <>
+        <Navbar/>
+        <Routes>
+          <Route exact path= "/" element= {<Home/>}/>
+          <Route exact path= "/about" element= {<About/>}/>
+          <Route exact path= "/service" element= {<Service/>}/>
+          <Route exact path= "/contact" element= {<Contact/>}/>
+          <Route to= "/" />
+        </Routes>
+
+
+        </>
+    )
+}
+
+export default Application;
